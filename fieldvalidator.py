315fa4268ba1c025445a8058a474e144947b5ffd
Nandani <nandani15p@gmail.com>
2025-06-10 23:17:08 +0530

    feat: create modelvalidator.py

--- fieldvalidator.py
+++ fieldvalidator.py
@@ -1,47 +1,53 @@
-from pydantic import BaseModel,EmailStr,AnyUrl,Field,field_validator
-from typing import List,Dict,Optional,Annotated
-# now here this is schema that we are defining
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
+from typing import List, Dict, Optional, Annotated
+
 class Patient(BaseModel):
-  ## here we doing type validation with the help of pydantic
-    
-    name:str
-    email:EmailStr
-    age:int
-    weight:float
-    married:bool
-    #   Optional module can also be used.(none is a default value)
-    allergies:List[str]
+
+    name: str
+    email: EmailStr
+    age: int
+    weight: float
+    married: bool
+    allergies: List[str]
     contact_details: Dict[str, str]
-    
+
     @field_validator('email')
     @classmethod
-    def email_validator(cls,value):
-        
-        valid_domains=['hdfc.com','icici.com']
-        #abc@gmail.com
-        domain_name=value.split('@')[-1]
-        
+    def email_validator(cls, value):
+
+        valid_domains = ['hdfc.com', 'icici.com']
+        # abc@gmail.com
+        domain_name = value.split('@')[-1]
+
         if domain_name not in valid_domains:
             raise ValueError('Not a valid domain')
+
         return value
+    
     @field_validator('name')
     @classmethod
-    def transform_name(cls,value):
+    def transform_name(cls, value):
         return value.upper()
     
-    
+    @field_validator('age', mode='after')
+    @classmethod
+    def validate_age(cls, value):
+        if 0 < value < 100:
+            return value
+        else:
+            raise ValueError('Age should be in between 0 and 100')
 
-def insert_patient_data(patient: Patient):
+
+def update_patient_data(patient: Patient):
+
     print(patient.name)
     print(patient.age)
-    print("inserted")
-def insert_patient_data(patient: Patient):
-    print(patient.name)
-    print(patient.age)
-    print("updated")
-    
-    
-patient_info={'name':'John Doe','age':30,'weight':75.2,'married':True,'allergies':['pollen','dust'],'contact_details':{'email':'abc@hdfc.com','phone':'123456'}}
-patient1=Patient(**patient_info)
+    print(patient.allergies)
+    print(patient.married)
+    print('updated')
 
-insert_patient_data(patient1)
+patient_info = {'name':'nitish', 'email':'abc@icici.com', 'age': '30', 'weight': 75.2, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details':{'phone':'2353462'}}
+
+patient1 = Patient(**patient_info) # validation -> type coercion
+
+update_patient_data(patient1)
